c5bce220861d7478b72940ef8a1bdb607d2f5566
Evgenii Kalkutin <evgheni.calcutin@gmail.com>
2025-04-22 19:35:16 +0300

    feat(): Add warn method

--- src/logger/abstract/base-logger.ts
+++ src/logger/abstract/base-logger.ts
@@ -1,4 +1,5 @@
 export abstract class BaseLogger {
   abstract error(message: string, meta?: object): void;
+  abstract warn(message: string, meta?: object): void;
   abstract info(message: string, meta?: object): void;
 }
