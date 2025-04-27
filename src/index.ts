150122505a940dd84e7813ee70f216afdc0ea027
Evgenii Kalkutin <evgheni.calcutin@gmail.com>
2025-04-27 06:17:39 +0300

    feat(): Expose LogContext

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './exceptions';
 
 export { LoggerFactory, LogLevel } from './logger';
-export type { LoggerOptions } from './logger';
+export type * from './logger';
 
 export type { Logger } from './logger';
--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,7 +1,6 @@
-import { LogLevel, LoggerOptions, Logger } from './core';
+import { LogLevel, LoggerOptions, Logger, LogContext } from './core';
 import { LoggerFactory } from './factory';
 
-export type { Logger };
-export type { LoggerOptions };
+export type { LogContext, LoggerOptions, Logger };
 export { LogLevel };
 export { LoggerFactory };
