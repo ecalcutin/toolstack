90fddad85e5305fa2ec24a12fba3ce8a7638dde5
Evgenii Kalkutin <evgheni.calcutin@gmail.com>
2025-04-28 17:44:19 +0300

    feat(): Adjust simplicity

--- src/logger/transports/console/console.transport.ts
+++ src/logger/transports/console/console.transport.ts
@@ -10,13 +10,14 @@ export class ConsoleTransport implements Transport {
       context?: LogContext;
     },
   ): void {
-    const method = this.getConsoleMethod(level);
-    method(this.formatter.format(level, raw.message, raw.context));
+    const method = this.getLogMethod(level);
+    const message = this.formatter.format(level, raw.message, raw.context);
+    method(message);
   }
 
   /* eslint-disable no-console  */
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any  */
-  private getConsoleMethod(level: string): (...args: any[]) => void {
+  private getLogMethod(level: string): (...args: any[]) => void {
     switch (level) {
       case 'error':
         return console.error;
